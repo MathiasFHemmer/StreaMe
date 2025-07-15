af4c2ad5ddc7aa96c202dfef485c970e285a9883
Mathias Hemmer <mathias.f.hemmer@gmail.com>
2025-07-15 08:37:02 -0300

    removed prometheus, added tempo and made it work, added loki

--- api/Program.cs
+++ api/Program.cs
@@ -4,6 +4,8 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Trace;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +19,10 @@ builder.Services.AddHangfire(c =>
 
 builder.Services.AddHangfireServer();
 
-var serviceName = "streame";
+var serviceName = Assembly.GetEntryAssembly()?.GetName().Name ?? "unknown";
 var serviceVersion = "1.0.0";
+
+builder.Services.AddSingleton(new ActivitySource(serviceName));
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(
@@ -26,13 +30,17 @@ builder.Services.AddOpenTelemetry()
         serviceVersion: serviceVersion))
     .WithTracing(tracing => tracing
         .AddSource(serviceName)
-        .AddAspNetCoreInstrumentation()
-        .AddConsoleExporter())
-    .WithMetrics(metrics => metrics
-        .AddMeter(serviceName)
-        .AddAspNetCoreInstrumentation()
-        .AddPrometheusExporter()
+        .AddAspNetCoreInstrumentation(          )
+        .AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri("http://alloy:4317");
+        })
         .AddConsoleExporter());
+    // .WithMetrics(metrics => metrics
+    //     .AddMeter(serviceName)
+    //     .AddAspNetCoreInstrumentation()
+    //     .AddPrometheusExporter()
+    //     .AddConsoleExporter());
 
 builder.Logging.AddOpenTelemetry(options => options
     .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
@@ -47,16 +55,19 @@ builder.Logging.AddOpenTelemetry(options => options
 builder.Services.AddTransient<VideoEncodingJob>();
 builder.Services.AddTransient<JobStateTracker>();
 
-
 var app = builder.Build();
 
-app.UseOpenTelemetryPrometheusScrapingEndpoint("/metrics");
 app.UseHangfireDashboard("/hangfire", new DashboardOptions
 {
     Authorization = [new HangfireAuthFilter()]
 });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] ActivitySource activitySource, [FromServices] ILogger<Program> logger) =>
+{
+    using var activity = activitySource.StartActivity("EncodeVideo");
+    logger.LogInformation("Testing");
+    return "Hello World!";
+});
 app.MapPost("/encode", (
     [FromBody] EncodeRequest request, 
     [FromServices] VideoEncodingJob job, 
